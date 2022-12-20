60a8bba2c2abfad949ef13621ab4663ed3b4d67b
Prasham1710 <jainprasham17@gmail.com>
2022-12-20 16:26:54 +0530

    new

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='text-black'>
-        <h1>jai jinendra</h1>
+        <h1 className='w-full text-2xl font-bold text-blue-500'>Data pack configuation</h1>
     </div>
   )
 }
