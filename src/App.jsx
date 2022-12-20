8759b0beb3571616280e0b16544809457c40e13b
Prasham1710 <jainprasham17@gmail.com>
2022-12-20 22:30:26 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Hero from './componets/Hero'
 import Navbar from './componets/Navbar'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   )
 }
