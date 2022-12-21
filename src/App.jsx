bc495a4655734340586f9321e8f04b7aeb6b2c17
Prasham1710 <jainprasham17@gmail.com>
2022-12-21 09:32:54 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Hero from './componets/Hero'
 import Navbar from './componets/Navbar'
 import Analytics from './componets/analytics'
-
+import Notify from './componets/Notify'
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
       <Analytics />
+      <Notify />
     </div>
   )
 }
--- src/componets/analytics.jsx
+++ src/componets/analytics.jsx
@@ -5,6 +5,14 @@ const analytics = () => {
     <div className='w-full bg-white py-16 px-4'>
         <div className='max-w[1240px] mx- auto grid md:grid-cols-2'>
             <img src={Laptop} alt="/" />
+            <div>
+                <p> Data analysis for investment</p>
+                <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Manage Data Analytics Centrally</h1>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
+            molestiae delectus culpa hic assumenda, voluptate reprehenderit
+            dolore autem cum ullam sed odit perspiciatis</p>
+             <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3'>Get Started</button>
+            </div>
         </div></div>
   )
 }
