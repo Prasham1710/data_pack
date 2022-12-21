30b277799d4ac1861637a1ba1c2de108ba89a36e
Prasham1710 <jainprasham17@gmail.com>
2022-12-21 12:04:01 +0530

    n

--- src/componets/Cards.jsx
+++ src/componets/Cards.jsx
@@ -9,13 +9,13 @@ const Cards = () => {
          <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
       <img className='w-20 mx-auto mt-[-3rem] bg-white ' src={Single} alt="/"/>
       <h2 className='text-2xl font-bold text-center py-8'>Single User</h2>
-      <p className='text-centre text-4xl font-bold'>Rs 249</p>
-      <div className='text-centre font-medium'>
-        <p> 1gb/Day</p>
-        <p>unlimited voice calls</p>
-        <p>100sms/Day</p>
-      </div>
-      <button className='bg-[#8cf175] w-[200px] rounded-md font-medium my-6 mx-auto py-2 text-black'>Buy Now</button>
+       <p className='text-center text-4xl font-bold'>Rs149</p>
+      <div className='text-center font-medium'>
+                  <p className='py-2 border-b mx-8 mt-8'>1 GB/Day</p>
+                  <p className='py-2 border-b mx-8'>Unlimited voice calls</p>
+                  <p className='py-2 border-b mx-8'>100 sms/day</p>
+              </div>
+      <button className='bg-[#8cf175] w-[200px] rounded-md font-medium my-6 mx-auto py-2 text-black'><a href = 'https://www.jio.com/mobile.html#fiber'>buy now</a></button>
       </div>
       </div>
     </div>
