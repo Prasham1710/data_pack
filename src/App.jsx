6e4bd7c0497dc9d5da76f9bf85d8e27b8d9671f9
Prasham1710 <jainprasham17@gmail.com>
2022-12-22 20:24:14 +0530

    n

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Hero from './componets/Hero'
 import Navbar from './componets/Navbar'
-import About from './componets/About'
+import Analytics from './componets/analytics'
 import Notify from './componets/Notify'
 import Cards from './componets/Cards'
 import Footer from './componets/Footer'
@@ -12,7 +12,7 @@ const App = () => {
       <Navbar />
       <BrowserRouter > 
       <Routes>
-        <Route path='/about' element= {<About/>}/>
+        <Route path='/about' element= {<Analytics/>}/>
         <Route path='/' element= {<Hero/>}/>
         <Route path='/help' element= {<Notify/>}/>
         <Route path='/packages' element= {<Cards/>}/>
@@ -21,7 +21,10 @@ const App = () => {
       </BrowserRouter>
       
       
-     
+      <Analytics/>
+      <Notify />
+      <Cards />
+      <Footer/>
     </div>
   )
 }
