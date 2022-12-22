85bd1233ac422986df7edce0548888542cacbe7d
Prasham1710 <jainprasham17@gmail.com>
2022-12-22 23:07:18 +0530

    n

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <div>
       <Navbar />
-      {/* <BrowserRouter > 
+      <BrowserRouter > 
       <Routes>
         <Route path='/about' element= {<Analytics/>}/>
         <Route path='/' element= {<Hero/>}/>
@@ -18,13 +18,9 @@ const App = () => {
         <Route path='/packages' element= {<Cards/>}/>
         <Route path='/contact' element= {<Footer/>}/>
       </Routes>
-      </BrowserRouter> */}
+       </BrowserRouter>
       
-      <Hero />
-      <Analytics/>
-      <Notify />
-      <Cards />
-      <Footer/>
+      
     </div>
   )
 }
